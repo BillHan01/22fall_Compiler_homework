b42550cb1a3f5f909ddc5589fa53796782752bcd
Billlllllllan <80232148+BillHan01@users.noreply.github.com>
2022-11-23 21:50:03 +0800

    Update LL1.cpp
    +add function of makeFollow to create and show FOLLOW set, the function of FIRST and FOLLOW are both avaliable now. 
    But there's still something that i'm not quite clear during the algorithm. Maybe I should try to understand it again.

--- LL1.cpp
+++ LL1.cpp
@@ -32,8 +32,6 @@ Production::Production(string str)
         }
         left += str[i];
     }
-    if (!vn_dic[left])
-        vn_dic[left] = vn_set.size();       //索引从1开始
     /*扫描产生式右边，复制进right中*/
     string tmp = "";
     while (i < len)
@@ -71,7 +69,6 @@ map<string, set<char>> first;     //firsr集合
 map<string, set<char>> follow;    //follow集合
 int vn_is_visited[MAX_VALUE] = { 0 };
 
-
 /*每次分析前，清空所有容器中的变量*/
 void cleanData()
 {
@@ -82,6 +79,17 @@ void cleanData()
     memset(vn_is_visited, 0, sizeof(vn_is_visited));
 }
 
+/*初始化visit标志数组*/
+void initVisit(vector<Production>& vn_set)
+{
+    string left;
+    for (int i = 0; i < vn_set.size(); i++)
+    {
+        left = vn_set[i].left;
+        if (!vn_dic[left])
+            vn_dic[left] = i + 1;       //索引从1开始
+    }
+}
 
 /*深度优先遍历构造FIRST*/
 void DFS(int i)
@@ -104,7 +112,7 @@ void DFS(int i)
             if (isupper(iter->at(j)))  //若当前指向非终结符
             {
                 int k;
-                if (j != iter->length() - 1 && iter->at(j + 1) == '\'')  //这里没太看懂。。。。。。。。。。。。
+                if (j != iter->length() - 1 && iter->at(j + 1) == '\'')  //考虑非终结符形式为 P'
                     k = vn_dic[iter->substr(j, 2)] - 1;
                 else
                     k = vn_dic[iter->substr(j, 1)] - 1;
@@ -129,12 +137,181 @@ void DFS(int i)
         }
 }
 
-
-
 /*构造并输出FIRST集合功能函数*/
 void makeFirst()
 {
-    
+    for (int i = 0; i < vn_set.size(); i++)
+        DFS(i);
+
+    cout << endl;
+    cout << "******************** FIRST set ********************" << endl;
+    map<string, set<char>>::iterator iter = first.begin();
+    for (; iter != first.end(); iter++)
+    {
+        cout << "FIRST(" << iter->first.c_str() << ") = {";
+        set<char>& tmp = iter->second;
+        set<char>::iterator iter_t = tmp.begin();
+        bool flag = false;
+        for (; iter_t != tmp.end(); iter_t++)
+        {
+            if (flag == true)
+                cout << ",";
+            cout << *iter_t;
+            flag = true;
+        }
+        cout << " }" << endl;
+    }
+    cout << endl;
+}
+
+/*构造FOLLOW集合过程中，将str1的FOLLOW元素加到str2中*/
+void append(const string& str1, const string& str2)
+{
+    set<char>& s1 = follow[str1];
+    set<char>& s2 = follow[str2];
+    set<char>::iterator iter = s1.begin();
+    for (; iter != s1.end(); iter++)
+        s2.insert(*iter);
+}
+
+/*构造并输出FOLLOW集合功能函数*/
+void makeFollow()
+{
+    while (1)
+    {
+        int goon = 0;
+        for (int i = 0; i < vn_set.size(); i++)
+        {
+            string& left = vn_set[i].left;
+            set<string>& right = vn_set[i].right;
+            set<string>::iterator iter = right.begin();
+            for (; iter != right.end(); iter++)
+            {
+                int flag = 1;
+                const string& str = *iter;
+                for (int j = iter->length() - 1; j >= 0; j--)
+                {
+                    if (str[j] == '\'')
+                    {
+                        int index = vn_dic[iter->substr(j - 1, 2)] - 1;
+                        if (flag == 1)
+                        {
+                            int tt = follow[iter->substr(j - 1, 2)].size();
+                            append(left, iter->substr(j - 1, 2));
+                            int tt1 = follow[iter->substr(j - 1, 2)].size();
+                            if (tt1 > tt) 
+                                goon = true;
+                            if (!vn_set[index].right.count("~"))
+                                flag = false;
+                        }
+
+                        
+                        for (int k = j + 1; k < iter->length(); k++)
+                        {
+                            if (isupper(str[k]))
+                            {
+                                string id;
+                                if (k != iter->length() - 1 && str[k + 1] == '\'')
+                                    id = iter->substr(k, 2);
+                                else id = iter->substr(k, 1);
+                                set<char>& from = first[id];
+                                set<char>& to = follow[iter->substr(j - 1, 2)];
+                                int tt = to.size();
+                                set<char>::iterator it1 = from.begin();
+                                for (; it1 != from.end(); it1++)
+                                    if (*it1 != '~')
+                                        to.insert(*it1);
+                                int tt1 = follow[iter->substr(j - 1, 2)].size();
+                                if (tt1 > tt) goon = true;
+                                if (!vn_set[vn_dic[id] - 1].right.count("~"))
+                                    break;
+                            }
+                            else if (str[k] != '\'')
+                            {
+                                int tt = follow[iter->substr(j - 1, 2)].size();
+                                follow[iter->substr(j - 1, 2)].insert(str[k]);
+                                int tt1 = follow[iter->substr(j - 1, 2)].size();
+                                if (tt1 > tt)
+                                    goon = true;
+                                break;
+                            }
+                            else continue;
+                        }
+                        j--;
+                    }
+                    else if (isupper(str[j]))
+                    {
+                        int x = vn_dic[iter->substr(j, 1)] - 1;
+                        if (flag)
+                        {
+                            int tt = follow[iter->substr(j, 1)].size();
+                            append(left, iter->substr(j, 1));
+                            if (!vn_set[x].right.count("~"))
+                                flag = false;
+                            int tt1 = follow[iter->substr(j, 1)].size();
+                            if (tt1 > tt) 
+                                goon = true;
+                        }
+                        for (int k = j + 1; k < iter->length(); k++)
+                        {
+                            if (isupper(str[k]))
+                            {
+                                string id;
+                                if (k != iter->length() - 1 && str[k + 1] == '\'')
+                                    id = iter->substr(k, 2);
+                                else id = iter->substr(k, 1);
+                                set<char>& from = first[id];
+                                set<char>& to = follow[iter->substr(j, 1)];
+                                set<char>::iterator it1 = from.begin();
+                                int tt = follow[iter->substr(j, 1)].size();
+                                for (; it1 != from.end(); it1++)
+                                    if (*it1 != '~')
+                                        to.insert(*it1);
+                                int tt1 = follow[iter->substr(j, 1)].size();
+                                if (tt1 > tt) goon = true;
+                                if (!vn_set[vn_dic[id] - 1].right.count("~"))
+                                    break;
+                            }
+                            else if (str[k] != '\'')
+                            {
+                                int tt = follow[iter->substr(j, 1)].size();
+                                follow[iter->substr(j, 1)].insert(str[k]);
+                                int tt1 = follow[iter->substr(j, 1)].size();
+                                if (tt1 > tt) 
+                                    goon = true;
+                                break;
+                            }
+                            else continue;
+                        }
+                    }
+                    else flag = false;
+                }
+            }
+        }
+        if (goon == 0)
+            break;
+    }
+
+    cout << endl;
+    cout << "******************** FOLLOW set ********************" << endl;
+    map<string, set<char> >::iterator iter = follow.begin();
+    for (; iter != follow.end(); iter++)
+    {
+        cout << "FOLLOW(" << iter->first.c_str() << ")={ ";
+        set<char>& tmp = iter->second;
+        tmp.insert('#');
+        set<char>::iterator iter_t = tmp.begin();
+        bool flag = false;
+        for (; iter_t != tmp.end(); iter_t++)
+        {
+            if (flag == 1)
+                cout << ",";
+            cout << *iter_t;
+            flag = 1;
+        }
+        cout << " }" << endl;
+    }
+    cout << endl;
 }
 
 
@@ -164,7 +341,9 @@ int main()
             Production pro(str);
             vn_set.push_back(pro);
         }
-
+        initVisit(vn_set);
+        makeFirst();
+        makeFollow();
     }
     return 0;
 }
